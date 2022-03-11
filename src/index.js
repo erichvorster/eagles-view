1ae9dff800d9e3dec934f9cc2781518ad6820c45
erichvorster <erichvorster1@gmail.com>
2022-03-11 21:38:36 +0200

    1st commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import * as bootstrap from 'bootstrap';
+
+const left = document.querySelector('.left');
+const right = document.querySelector('.right');
+const container = document.querySelector('.split-container')
+
+
+left.addEventListener('mouseenter', () => {
+    container.classList.add('hover-left');
+})
+
+left.addEventListener('mouseleave', () => {
+    container.classList.remove('hover-left')
+})
+
+right.addEventListener('mouseenter', () => {
+    container.classList.add('hover-right');
+})
+
+right.addEventListener('mouseleave', () => {
+    container.classList.remove('hover-right')
+})
